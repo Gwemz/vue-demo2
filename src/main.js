1541b5e22cd830a1f10c6b78bfca42c6024748e3
guowentao <gwem.win@gmail.com>
2018-01-02 10:01:40 +0800

    'test'

--- src/main.js
+++ src/main.js
@@ -1,36 +1,24 @@
-// 0. 如果使用模块化机制编程，導入Vue和VueRouter，要调用 Vue.use(VueRouter)
+import Vue from 'vue'
+import routes from './routes'
 
-// 1. 定义（路由）组件。
-// 可以从其他文件 import 进来
-// import Default from '@components/Default'
-// import Foo from '@components/Foo'
-// import Bar from '@components/Bar'
-const Default = { template: '<div>这是默认内容</div>' }
-const Foo = { template: '<div>这是首页内容</div>' }
-const Bar = { template: '<div>关于我们内容</div>' }
-
-// 2. 定义路由
-// 每个路由应该映射一个组件。 其中"component" 可以是
-// 通过 Vue.extend() 创建的组件构造器，
-// 或者，只是一个组件配置对象。
-// 我们晚点再讨论嵌套路由。
-const routes = [
-    { path: '/', component: Default },
-    { path: '/index', component: Foo },
-    { path: '/about', component: Bar }
-]
-
-// 3. 创建 router 实例，然后传 `routes` 配置
-// 你还可以传别的配置参数, 不过先这么简单着吧。
-const router = new VueRouter({
-    routes // （缩写）相当于 routes: routes
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/404.vue')
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent)
+  }
 })
 
-// 4. 创建和挂载根实例。
-// 记得要通过 router 配置参数注入路由，
-// 从而让整个应用都有路由功能
-const app = new Vue({
-    router
-}).$mount('#app')
-
-// 现在，应用已经启动了！
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+})
