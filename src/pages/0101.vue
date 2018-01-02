4afe389a0888ce87cc0d1e200b49f766b06fde45
guowentao <gwem.win@gmail.com>
2018-01-02 11:25:33 +0800

    'vue'

---
+++ src/pages/0101.vue
@@ -0,0 +1,15 @@
+<template>
+    <main-layout>
+        <p>0101 page</p>
+    </main-layout>
+</template>
+
+<script>
+    import MainLayout from '../layouts/Main.vue'
+
+    export default {
+        components: {
+            MainLayout
+        }
+    }
+</script>
---
+++ src/pages/0102.vue
@@ -0,0 +1,15 @@
+<template>
+    <main-layout>
+        <p>0102 page</p>
+    </main-layout>
+</template>
+
+<script>
+    import MainLayout from '../layouts/Main.vue'
+
+    export default {
+        components: {
+            MainLayout
+        }
+    }
+</script>
