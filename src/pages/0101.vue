2643a103d2369c87bb1b689c0cd021fc0e8db121
guowentao <gwem.win@gmail.com>
2018-01-03 15:56:06 +0800

    test

--- src/pages/0101.vue
+++ src/pages/0101.vue
@@ -1,8 +1,15 @@
 <template>
     <main-layout>
-        <p>This is 0101 page</p>
+        <p>这是项目第一页</p>
     </main-layout>
 </template>
+
+<style>
+p{
+    color: rgb(0, 255, 170);
+}
+</style>
+
 
 <script>
     import MainLayout from '../layouts/Main.vue'
--- src/pages/0102.vue
+++ src/pages/0102.vue
@@ -1,8 +1,14 @@
 <template>
     <main-layout>
-        <p>This is 0102 page</p>
+        <p>这是项目第二页</p>
     </main-layout>
 </template>
+<style>
+p{
+    color:rgb(226, 21, 21);
+}
+</style>
+
 
 <script>
     import MainLayout from '../layouts/Main.vue'
