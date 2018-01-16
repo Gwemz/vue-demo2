025cf8a6a4fe0f627f56aaf746d12222d89d9040
guowentao <gwem.win@gmail.com>
2018-01-16 10:50:30 +0800

    'test'

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -133,49 +133,49 @@
             items:[
                 {"title":"第一章 表单常用代码",
                 "data":[
-                    {url: '/0101', title: '去除字符串左右的空格'},
-                    {url: '/0102', title: '验证用户是否输入'},
-                    {url: '/0103', title: '禁止用户输入'},
-                    {url: '/0104', title: '关闭用户中文输入法'},
-                    {url: '/0105', title: '禁止用户复制和粘贴'},
-                    {url: '/0106', title: '限制只能输入数字'},
-                    {url: '/0107', title: '限制字符串长度'},
-                    {url: '/0108', title: '拖拽进度条'},
-                    {url: '/0109', title: '通过cookie实现网页背景切换'},
-                    {url: '/0110', title: '全国城市三级联动'},
-                    {url: '/0111', title: 'Angular数据渲染'},
-                    {url: '/0112', title: 'JS生成图片验证码'}
+                    {url: '/vue-demo2/0101', title: '去除字符串左右的空格'},
+                    {url: '/vue-demo2/0102', title: '验证用户是否输入'},
+                    {url: '/vue-demo2/0103', title: '禁止用户输入'},
+                    {url: '/vue-demo2/0104', title: '关闭用户中文输入法'},
+                    {url: '/vue-demo2/0105', title: '禁止用户复制和粘贴'},
+                    {url: '/vue-demo2/0106', title: '限制只能输入数字'},
+                    {url: '/vue-demo2/0107', title: '限制字符串长度'},
+                    {url: '/vue-demo2/0108', title: '拖拽进度条'},
+                    {url: '/vue-demo2/0109', title: '通过cookie实现网页背景切换'},
+                    {url: '/vue-demo2/0110', title: '全国城市三级联动'},
+                    {url: '/vue-demo2/0111', title: 'Angular数据渲染'},
+                    {url: '/vue-demo2/0112', title: 'JS生成图片验证码'}
                 ]},
                 {"title":"第二章 JS效果相关代码",
                 "data":[
-                    {url: '/0201', title: '楼层跳转效果'},
-                    {url: '/0202', title: '按需加载图片'},
-                    {url: '/0203', title: '全选、反选、全不选效果'},
-                    {url: '/0204', title: '图片懒加载'},
-                    {url: '/0205', title: '照片查看器'},
-                    {url: '/0206', title: '图片轮播'},
-                    {url: '/0207', title: '图片上传'},
-                    {url: '/0208', title: '分页功能实现'}
+                    {url: '/vue-demo2/0201', title: '楼层跳转效果'},
+                    {url: '/vue-demo2/0202', title: '按需加载图片'},
+                    {url: '/vue-demo2/0203', title: '全选、反选、全不选效果'},
+                    {url: '/vue-demo2/0204', title: '图片懒加载'},
+                    {url: '/vue-demo2/0205', title: '照片查看器'},
+                    {url: '/vue-demo2/0206', title: '图片轮播'},
+                    {url: '/vue-demo2/0207', title: '图片上传'},
+                    {url: '/vue-demo2/0208', title: '分页功能实现'}
                 ]},
                 {"title":"第三章 js相关插件",
                 "data":[
-                    {url: '/0301', title: 'PhotoSwipe 相册控件'},
-                    {url: '/0302', title: 'fullpage-网易邮箱6.0'},
-                    {url: '/0303', title: 'fullpage-iPhone 5C'},
-                    {url: '/0304', title: 'three.js-3D图形绘制'},
-                    {url: '/0305', title: 'Video.js-视频播放器'},
-                    {url: '/0306', title: 'sweetalert2-Alert弹框'},
-                    {url: '/0307', title: 'Chart.js'},
-                    {url: '/0308', title: 'dragsort拖拽排序'},
-                    {url: '/0309', title: 'jquery-qrcode图片二维码'},
-                    {url: '/0310', title: 'JS进行MD5加密'}
+                    {url: '/vue-demo2/0301', title: 'PhotoSwipe 相册控件'},
+                    {url: '/vue-demo2/0302', title: 'fullpage-网易邮箱6.0'},
+                    {url: '/vue-demo2/0303', title: 'fullpage-iPhone 5C'},
+                    {url: '/vue-demo2/0304', title: 'three.js-3D图形绘制'},
+                    {url: '/vue-demo2/0305', title: 'Video.js-视频播放器'},
+                    {url: '/vue-demo2/0306', title: 'sweetalert2-Alert弹框'},
+                    {url: '/vue-demo2/0307', title: 'Chart.js'},
+                    {url: '/vue-demo2/0308', title: 'dragsort拖拽排序'},
+                    {url: '/vue-demo2/0309', title: 'jquery-qrcode图片二维码'},
+                    {url: '/vue-demo2/0310', title: 'JS进行MD5加密'}
                 ]},
                 {"title":"第四章 CSS3效果层",
                 "data":[
-                    {url: '/0404', title: 'css3弹出层'},
-                    {url: '/0405', title: 'Yo-master相关动画'},
-                    {url: '/0406', title: 'CSS使用Tips'},
-                    {url: '/0407', title: 'CSS3加载动画'}
+                    {url: '/vue-demo2/0404', title: 'css3弹出层'},
+                    {url: '/vue-demo2/0405', title: 'Yo-master相关动画'},
+                    {url: '/vue-demo2/0406', title: 'CSS使用Tips'},
+                    {url: '/vue-demo2/0407', title: 'CSS3加载动画'}
                 ]},
                 {"title":"第五章 移动端效果",
                 "data":[
@@ -183,16 +183,16 @@
                 ]},
                 {"title":"第六章 时间日期效果",
                 "data":[
-                    {url: '/0601', title: '学习资源整理(By phodal Huang)'},
-                    {url: '/0602', title: 'IntelliJ-IDEA'}
+                    {url: '/vue-demo2/0601', title: '学习资源整理(By phodal Huang)'},
+                    {url: '/vue-demo2/0602', title: 'IntelliJ-IDEA'}
                 ]},
                 {"title":"第七章 其它练习",
                 "data":[
-                    {url: '/0701', title: '刷新当前页面'},
-                    {url: '/0702', title: '百度地图引用'},
-                    {url: '/0703', title: '高德地图引用'},
-                    {url: '/0704', title: '腾讯地图引用'},
-                    {url: '/0705', title: '扫码登录'}
+                    {url: '/vue-demo2/0701', title: '刷新当前页面'},
+                    {url: '/vue-demo2/0702', title: '百度地图引用'},
+                    {url: '/vue-demo2/0703', title: '高德地图引用'},
+                    {url: '/vue-demo2/0704', title: '腾讯地图引用'},
+                    {url: '/vue-demo2/0705', title: '扫码登录'}
                 ]}
             ]
         }
