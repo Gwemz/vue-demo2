0b2e5234d1314d6e35a8e284ea689e4e21f41aee
guowentao <gwem.win@gmail.com>
2018-01-22 10:35:49 +0800

    'test'

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-view></router-view>
+</template>
+<script>
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+import Main from './components/main.vue'
+export default {
+  name:'app',
+  components:{
+      Home,
+      About,
+      Main
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,35 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Main from './components/main.vue'
-import Home from './pages/Home.vue'
-import About from './pages/About.vue'
+import routes from './routes'
 
-
-// 1. Use plugin.
-// This installs <router-view> and <router-link>,
-// and injects $router and $route to all router-enabled child components
 Vue.use(VueRouter)
 
-// 2. Define route components
-// const Home = { template: '<div>home</div>' }
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-// 3. Create the router
 const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes: [
-    { path: '/', component: Home },
-    { path: '/foo', component: Foo }, 
-    { path: '/bar', component: Bar },
-    { path: '/main', component: Main }
-  ]
+  routes
 })
 
-// 4. Create and mount root instance.
-// Make sure to inject the router.
-// Route components will be rendered inside <router-view>.
 new Vue({
   router
 }).$mount('#app')
